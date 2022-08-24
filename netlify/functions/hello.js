c71b8b64bcc85a0b96e744961940006ad79023ba
Per Harald Borgen <perhborgen@gmail.com>
2022-08-24 12:29:00 +0200

    remove more twilio

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -5,23 +5,8 @@
 // You can develop and deploy serverless functions right here as part
 // of your site. Netlify Functions will handle the rest for you.
 
-
-
-const accountSid = 'AC4612f46561a10f597a22d3f9ddae7aa9';
-const authToken = '2c6dc18c5f5ad37f94711e13ecc31ef9';
-const client = require('twilio')(accountSid, authToken);
-
-
 exports.handler = async event => {
 
-    client.messages
-    .create({
-        body: 'This is my first node generated SMS!',
-        from: '+1 218 392 4714',
-        to: '+4797655557' // MUST BE A REGISTERED NUMBER, IE, YOUR OWN
-    })
-    .then(message => console.log(message.sid))
-    .catch(error => console.log("error", error))
 
     const subject = event.queryStringParameters.name || 'World'
     return {
